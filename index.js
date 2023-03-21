ea2edf28ee836b0dbb23953137efc119ae1ac6f1
Soungjae <soungj94@gmail.com>
2023-03-21 00:07:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(todo = "roller-skate") {
+  return `This Saturday, I want to ${todo}!`;
+}
+
+const mondayWork = function (todo = "go to the office") {
+  return `This Monday, I will ${todo}.`;
+};
+
+mondayWork();
+
+//
+
+function wrapAdjective(flair) {
+  return function (adj = "special") {
+    return `You are ${flair}${adj}${flair}!`;
+  };
+}
+
+// const encouragingPromptFunction = wrapAdjective("*");
+
+// console.log(encouragingPromptFunction("a hard worker")); // Output: "You are *a hard worker*!"
+// console.log(encouragingPromptFunction("a dedicated programmer")); // Output: "You are *a dedicated programmer*!"
+
+// const alternativeFlairFunction = wrapAdjective("||");
+
+// console.log(alternativeFlairFunction("a hard worker")); // Output: "You are ||a hard worker||!"
+// console.log(alternativeFlairFunction("a dedicated programmer")); // Output: "You are ||a dedicated programmer||!"
